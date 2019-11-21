1159c714b3ec80aa6d70fbcdc799928e630d8592
ombobbb <ombobbb88@gmail.com>
2019-11-21 18:11:27 +0700

    Belajar Flutter 2

--- Novice/02-02/Latihan/Layout in Flutter/materialapss/lib/main.dart
+++ Novice/02-02/Latihan/Layout in Flutter/materialapss/lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'dart:async' show Future;
+import 'package:flutter/services.dart' show rootBundle;
+
+Future<String> loadAsset() async {
+  return await rootBundle.loadString('assets/config.json');
+}
 
 void main() => runApp(MyApp());
 
